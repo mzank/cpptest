1363555c43875b4b832bd5db9778485ae1857311
Marco Zank <zank@gmx.at>
2026-03-13 09:33:06 +0100

    Add mdspan.cpp

---
+++ mdspan.cpp
@@ -0,0 +1,26 @@
+#include <mdspan>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+  std::vector<double> vec{2.4, 4.6, 6.2, -20.0, -5.4, -6.7};
+
+  std::cout << "vec : ";
+  for (auto x : vec)
+	  std::cout << x << "  ";
+  std::cout << "\n";
+
+  auto md =  std::mdspan(vec.data(),3,2);
+  std::cout << "md :\n";
+  for (int i = 0; i < md.extent(0); ++i)
+  {
+	  for (int j=0; j<md.extent(1); ++j)
+	  {
+		  std::cout << md[i,j] << "  ";
+	  }
+	  std::cout << "\n";
+  }
+  return EXIT_SUCCESS;
+}
+
